b073ed368afa08c0e349ead3a26d9e613bdc181c
WKBASFernando <sankalpaangelo@gmail.com>
2026-04-06 11:31:31 +0530

    zone client

---
+++ Zone-Management-Service/src/main/java/lk/ijse/Zone_Management_Service/client/ExternalIoTClient.java
@@ -0,0 +1,18 @@
+package lk.ijse.Zone_Management_Service.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import java.util.Map;
+
+@FeignClient(name = "external-iot-service", url = "${iot.external.base-url}")
+public interface ExternalIoTClient {
+
+    @PostMapping("/devices")
+    Map<String, Object> registerDevice(
+            @RequestHeader("Authorization") String token,
+            @RequestBody Map<String, String> payload
+    );
+}
