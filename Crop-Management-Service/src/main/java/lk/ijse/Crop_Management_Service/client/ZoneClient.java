784511093870e3f827bfa0025eeae1788cddb67f
WKBASFernando <sankalpaangelo@gmail.com>
2026-04-09 11:32:22 +0530

    crop zoneClient

---
+++ Crop-Management-Service/src/main/java/lk/ijse/Crop_Management_Service/client/ZoneClient.java
@@ -0,0 +1,11 @@
+package lk.ijse.Crop_Management_Service.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "ZONE-MANAGEMENT-SERVICE")
+public interface ZoneClient {
+    @GetMapping("/api/zones/{id}")
+    Object getZoneDetails(@PathVariable("id") Long id);
+}
